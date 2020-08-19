0caee16fb1fffc57bf09827328a25a0d843a8168
Shamal Sandeep <ssdh.gdrive@gmail.com>
2020-08-19 16:39:42 +0530

    Rebind to same port

--- tcp_hole_punching/Client.java
+++ tcp_hole_punching/Client.java
@@ -2,6 +2,15 @@ import java.net.*;
 
 public class Client {
     public static void main(String[] args) throws Exception {
-        Socket socket = new Socket("178.128.214.46", 5050);
+        Socket s = new Socket();
+        s.setReuseAddress(true);
+        s.bind(new InetSocketAddress("192.168.1.10", 9000));
+        s.connect(new InetSocketAddress("178.128.214.46", 5050));
+        s.close();
+
+        s = new Socket();
+        s.bind(new InetSocketAddress("192.168.1.10", 9000));
+        s.connect(new InetSocketAddress("178.128.214.46", 5050));
+        s.close();
     }
 }
--- tcp_hole_punching/Server.java
+++ tcp_hole_punching/Server.java
@@ -4,11 +4,13 @@ public class Server {
     public static void main(String[] args) throws Exception {
         ServerSocket server = new ServerSocket(5050);
 
-        Socket socket = server.accept();
-        InetAddress addr = socket.getInetAddress();
-        int port = socket.getPort();
+        while(true) {
+            Socket socket = server.accept();
+            InetAddress addr = socket.getInetAddress();
+            int port = socket.getPort();
 
-        System.out.println("Client IP: " + addr.toString());
-        System.out.println("Client Port: " + port);
+            System.out.println("Client IP: " + addr.toString());
+            System.out.println("Client Port: " + port);
+        }
     }
 }
